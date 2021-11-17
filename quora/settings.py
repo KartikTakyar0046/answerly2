41522ba9c2283b15ff9d3445c4b37e11738cf20d
kartik0046 <ktakyar_be18@thapar.edu>
2021-11-17 19:00:03 +0530

    init7

--- quora/settings.py
+++ quora/settings.py
@@ -168,7 +168,7 @@ STATIC_URL = '/static/'
 MEDIA_URL='/images/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 MEDIA_ROOT=os.path.join(BASE_DIR,'static/images')
